5c06f27270f5a7461f71fdb093f9bf2c5197a109
jnyyssol <jessetius@gmail.com>
2022-03-28 14:31:30 +0300

    removed old files, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DApp_example_geo
+
+Smart contract example. IDs of found geocaches are stored on Ganache blockchain through a simple interface. 
+
--- migrations/3_welcome_contract.js
+++
@@ -1,5 +0,0 @@
-const WelcomeContract = artifacts.require("WelcomeContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(WelcomeContract);
-};
