6c1cf5fffd5ea01721cafe81fd8edf1ae329cd19
Tuomas <tnummine21@student.oulu.fi>
2022-05-16 20:47:33 +0300

    poem.sol added

---
+++ contracts/poem.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract poemContract {
+    /* Purpose of this smart contract is to track poems based on address. Poem is identified as string
+    */
+
+    // structure of contract
+    struct poem {
+        uint authorId;
+        string poemAuthor;
+        string poemName;
+        string poemContent;
+    }
+
+    mapping (address => poem[]) myPoemList;
+    mapping (address => uint) counter;
+
+    constructor() public {
+        addPoem("Tekija", "nimi", "liirum laarum");
+    }
+
+    function checkPoemsNames(string memory _poemName) private view returns (bool) {
+        for (uint i = 0; i < myPoemList[msg.sender].length; i++) {
+            if (keccak256(abi.encodePacked(_poemName)) == keccak256(abi.encodePacked(myPoemList[msg.sender][i].poemName))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function addPoem(string memory _poemAuthor, string memory _poemName, string memory _poemcontent) public {
+        if(checkPoemsNames(_poemName)) {
+            counter[msg.sender]++;
+            myPoemList[msg.sender].push(poem(
+                counter[msg.sender],
+                _poemAuthor,
+                _poemName,
+                _poemcontent
+
+            ));
+        }
+    }
+
+    function poemById(string memory _poemName) private pure returns (string memory){
+        return (_poemName);
+    }
+
+}
